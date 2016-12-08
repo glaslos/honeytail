b5f2de1a7d904be853cc588049abed6e0118a074
glaslos <lukas.rist@bluecoat.com>
2016-12-08 16:43:10 +0100

    aws credentials added

--- honeytail.go
+++ honeytail.go
@@ -4,9 +4,11 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/a8m/kinesis-producer"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/kinesis"
 	"github.com/hpcloud/tail"
@@ -19,6 +21,8 @@ var (
 	fileWhence int
 	streamName string
 	shardName  string
+	awskey     string
+	awssecret  string
 )
 
 func process(pr *producer.Producer) {
@@ -46,14 +50,28 @@ func process(pr *producer.Producer) {
 }
 
 func main() {
+	flag.StringVar(&awskey, "awskey", os.Getenv("AWS_KEY"), "Set environment variable AWS_KEY or specify on prompt")
+	flag.StringVar(
+		&awssecret, "awssecret", os.Getenv("AWS_SECRET"),
+		"Set environment variable AWS_SECRET or specify on prompt",
+	)
 	flag.StringVar(&fileName, "file-name", "/var/log/syslog", "File to tail")
-	flag.StringVar(&streamName, "stream-name", "test-stream", "Kinesis stream to write to")
-	flag.StringVar(&shardName, "shard-name", "test-shard", "Kinesis shard to write to")
+	flag.StringVar(&streamName, "stream-name", "stream-test", "Kinesis stream to write to")
+	flag.StringVar(&shardName, "shard-name", "shard-test", "Kinesis shard to write to")
 	flag.BoolVar(&followFile, "follow-file", true, "Follow the file, tailing new lines")
 	flag.BoolVar(&reopenFile, "reopen-file", true, "Re-open the file if renamed (logrotate)")
 	flag.IntVar(&fileWhence, "file-whence", 2, "0: start, 1: current, 2:end")
 	flag.Parse()
-	client := kinesis.New(session.New(aws.NewConfig()))
+	creds := credentials.NewStaticCredentials(awskey, awssecret, "")
+	_, err := creds.Get()
+	if err != nil {
+		log.Println("Failed to create credentials", err.Error())
+	}
+	awsConfig := &aws.Config{
+		Region:      aws.String("eu-west-1"),
+		Credentials: creds,
+	}
+	client := kinesis.New(session.New(awsConfig))
 	pr := producer.New(&producer.Config{
 		StreamName:   streamName,
 		BacklogCount: 2000,
