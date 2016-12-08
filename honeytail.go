e81511d43e1724b2cabea2ff4227bd3af3d3fe70
glaslos <lukas.rist@bluecoat.com>
2016-12-08 14:25:25 +0100

    adding kinesis producer

--- honeytail.go
+++ honeytail.go
@@ -5,6 +5,10 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/a8m/kinesis-producer"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/kinesis"
 	"github.com/hpcloud/tail"
 )
 
@@ -13,9 +17,11 @@ var (
 	followFile bool
 	reopenFile bool
 	fileWhence int
+	streamName string
+	shardName  string
 )
 
-func process() {
+func process(pr *producer.Producer) {
 	t, err := tail.TailFile(fileName,
 		tail.Config{
 			Follow: followFile,
@@ -32,14 +38,28 @@ func process() {
 	for line := range t.Lines {
 		// Process log line
 		fmt.Println(line.Text)
+		err := pr.Put([]byte(line.Text), shardName)
+		if err != nil {
+			log.Println(err)
+		}
 	}
 }
 
 func main() {
 	flag.StringVar(&fileName, "file-name", "/var/log/syslog", "File to tail")
+	flag.StringVar(&streamName, "stream-name", "test-stream", "Kinesis stream to write to")
+	flag.StringVar(&shardName, "shard-name", "test-shard", "Kinesis shard to write to")
 	flag.BoolVar(&followFile, "follow-file", true, "Follow the file, tailing new lines")
 	flag.BoolVar(&reopenFile, "reopen-file", true, "Re-open the file if renamed (logrotate)")
 	flag.IntVar(&fileWhence, "file-whence", 2, "0: start, 1: current, 2:end")
 	flag.Parse()
-	process()
+	client := kinesis.New(session.New(aws.NewConfig()))
+	pr := producer.New(&producer.Config{
+		StreamName:   streamName,
+		BacklogCount: 2000,
+		Client:       client,
+	})
+
+	pr.Start()
+	process(pr)
 }
